2a3a8c9a84b76f3598af218568bd36ca134d6b70
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-01-10 20:29:36 -0300

    Testando os Commands

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTest.cs
@@ -0,0 +1,23 @@
+﻿
+using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Entities;
+
+
+namespace PaymentContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTest
+    {
+
+        [TestMethod]
+        public void ShouldreturnErroWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+
+            command.FirstName = "";
+            command.Validate();
+
+            Assert.AreEqual(false, command.IsValid);
+        }
+    }
+}
